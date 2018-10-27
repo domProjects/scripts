22951b0fdc4934a9338b41107e8270ec6c923b83
Emmanuel Campait <ecampait@gmail.com>
2018-10-27 12:09:18 +0200

    Update linux/ocsinventoryng.sh

--- linux/ocsinventoryng.sh
+++ linux/ocsinventoryng.sh
@@ -24,7 +24,7 @@ if [ $os_family = debian ]; then
   apt-get update && apt-get dist-upgrade
   # Install prereqs
   apt-get -y install apache2 \
-  php7.2 libapache2-mod-php7.2 \
+  php7.2 \
   php7.2-cgi php7.2-cli php7.2-curl php7.2-gd php7.2-json php7.2-ldap php7.2-mysql php7.2-opcache php7.2-snmp php7.2-xml php7.2-xmlrpc \
   php-pear \
   make gcc \
