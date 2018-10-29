3c123d1091f79cd782157ab5454eeae9ca3faead
Emmanuel Campait <ecampait@gmail.com>
2018-10-29 14:18:03 +0100

    Update linux/ocs-glpi.sh

--- linux/ocs-glpi.sh
+++ linux/ocs-glpi.sh
@@ -28,10 +28,27 @@ apt-get -y install apache2
 systemctl enable apache2
 systemctl start apache2
 
+#
+apt-get -y install php7.2
+apt-get -y install php7.2-curl
+apt-get -y install php7.2-mbstring
+apt-get -y install php7.2-snmp
+apt-get -y install php7.2-soap
+apt-get -y install php7.2-xml
+apt-get -y install libapache2-mod-perl2
 
-
-
-
+#
+cpan -i Apache::DBI
+cpan -i Archive::Zip
+cpan -i Compress::Zlib
+cpan -i DBD::Mysql
+cpan -i DBI
+cpan -i Mojolicious::Lite
+cpan -i Net::IP
+cpan -i Plack::Handler
+cpan -i XML::Entities
+cpan -i XML::Simple
+cpan -i YAML
 
 
 
