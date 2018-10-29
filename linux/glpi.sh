dde45dd2879ed6861c633e6cda413047831483a6
Emmanuel Campait <ecampait@gmail.com>
2018-10-29 18:23:12 +0100

    Update linux/glpi.sh

--- linux/glpi.sh
+++ linux/glpi.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 
-apt-get -y install php7.2
-apt-get -y install php7.2-apcu
-apt-get -y install php7.2-cgi
-apt-get -y install php7.2-curl
-apt-get -y install php7.2-gd
-apt-get -y install php7.2-imap
-apt-get -y install php7.2-intl
-apt-get -y install php7.2-ldap
-apt-get -y install php7.2-mbstring
-apt-get -y install php7.2-mysql
-apt-get -y install php7.2-pspell
-apt-get -y install php7.2-recode
-apt-get -y install php7.2-snmp
-apt-get -y install php7.2-soap
-apt-get -y install php7.2-tidy
-apt-get -y install php7.2-xml
-apt-get -y install php7.2-xmlrpc
-apt-get -y install php7.2-xsl
-apt-get -y install php-pear
-apt-get -y install php-cas
-apt-get -y install php-gettext
-apt-get -y install php-imagick
-apt-get -y install php-memcache
-apt-get -y install perl
-apt-get -y install perl-modules
-apt-get -y install libapache2-mod-php7.2
-apt-get -y install libapache2-mod-perl2
-apt-get -y install libcompress-zlib-perl
-apt-get -y install libdbd-mysql-perl
-apt-get -y install libdbi-perl
-apt-get -y install libnet-ip-perl
-apt-get -y install libsoap-lite-perl
-apt-get -y install libxml-simple-perl
+glpiversion="9.3.2"
+pluginglpiocsversion="1.5.4"
+
+wget -O glpi-${glpiversion}.tgz https://github.com/glpi-project/glpi/releases/download/${glpiversion}/glpi-${glpiversion}.tgz
+
+if [ $? -ne 0 ]; then
+  echo "Failed to download glpi-${glpiversion}.tgz"
+  echo "https://github.com/glpi-project/glpi/releases/download/${glpiversion}/glpi-${glpiversion}.tgz"
+  exit
+fi
+
+tar -xvf glpi-${glpiversion}.tgz
+mv glpi /var/www/html/
+chmod 755 -R /var/www/html/
+chown -R www-data:www-data /var/www/html/
+
+#
+wget -O glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz https://github.com/pluginsGLPI/ocsinventoryng/releases/download/${pluginglpiocsversion}/glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz
+
+if [ $? -ne 0 ]; then
+  echo "Failed to download glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz"
+  echo "https://github.com/pluginsGLPI/ocsinventoryng/releases/download/${pluginglpiocsversion}/glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz"
+  exit
+fi
+
+tar -xvf glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz
+mv ocsinventoryng /var/www/html/glpi/plugins/
