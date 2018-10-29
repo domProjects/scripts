7f487c01739332ac82c0fe4a65f9559df2b3e810
Emmanuel Campait <ecampait@gmail.com>
2018-10-29 18:17:36 +0100

    Update linux/glpi.sh

--- linux/glpi.sh
+++ linux/glpi.sh
@@ -1,51 +1,35 @@
 #!/bin/bash
 
-# glpi
-glpiversion="9.3.2"
-pluginglpiocsversion="1.5.4"
-
-ocsteamviewer="1.1"
-ocsnetworkshare="1.0"
-ocsofficepack="2.0"
-ocssecurity="1.1"
-
-
-
-#
-cd /tmp/
-
-#
-wget -O teamviewer.zip https://github.com/PluginsOCSInventory-NG/teamviewer/releases/download/${ocsteamviewer}/teamviewer.zip
-if [ $? -ne 0 ]; then
-    echo "Failed to download teamviewer.zip"
-    echo "https://github.com/PluginsOCSInventory-NG/teamviewer/releases/download/${ocsteamviewer}/teamviewer.zip"
-    exit
-fi
-mv teamviewer.zip /usr/share/ocsinventory-reports/ocsreports/download/
-
-#
-wget -O networkshare.zip https://github.com/PluginsOCSInventory-NG/networkshare/releases/download/${ocsnetworkshare}/networkshare.zip
-if [ $? -ne 0 ]; then
-    echo "Failed to download networkshare.zip"
-    echo "https://github.com/PluginsOCSInventory-NG/networkshare/releases/download/${ocsnetworkshare}/networkshare.zip"
-    exit
-fi
-mv networkshare.zip /usr/share/ocsinventory-reports/ocsreports/download/
-
-#
-wget -O officepack.zip https://github.com/PluginsOCSInventory-NG/officepack/releases/download/${ocsofficepack}/officepack.zip
-if [ $? -ne 0 ]; then
-    echo "Failed to download officepack.zip"
-    echo "https://github.com/PluginsOCSInventory-NG/officepack/releases/download/${ocsofficepack}/officepack.zip"
-    exit
-fi
-mv officepack.zip /usr/share/ocsinventory-reports/ocsreports/download/
-
-#
-wget -O security.zip https://github.com/PluginsOCSInventory-NG/security/releases/download/${ocssecurity}/security.zip
-if [ $? -ne 0 ]; then
-    echo "Failed to download security.zip"
-    echo "https://github.com/PluginsOCSInventory-NG/security/releases/download/${ocssecurity}/security.zip"
-    exit
-fi
-mv security.zip /usr/share/ocsinventory-reports/ocsreports/download/
+apt-get -y install php7.2
+apt-get -y install php7.2-apcu
+apt-get -y install php7.2-cgi
+apt-get -y install php7.2-curl
+apt-get -y install php7.2-gd
+apt-get -y install php7.2-imap
+apt-get -y install php7.2-intl
+apt-get -y install php7.2-ldap
+apt-get -y install php7.2-mbstring
+apt-get -y install php7.2-mysql
+apt-get -y install php7.2-pspell
+apt-get -y install php7.2-recode
+apt-get -y install php7.2-snmp
+apt-get -y install php7.2-soap
+apt-get -y install php7.2-tidy
+apt-get -y install php7.2-xml
+apt-get -y install php7.2-xmlrpc
+apt-get -y install php7.2-xsl
+apt-get -y install php-pear
+apt-get -y install php-cas
+apt-get -y install php-gettext
+apt-get -y install php-imagick
+apt-get -y install php-memcache
+apt-get -y install perl
+apt-get -y install perl-modules
+apt-get -y install libapache2-mod-php7.2
+apt-get -y install libapache2-mod-perl2
+apt-get -y install libcompress-zlib-perl
+apt-get -y install libdbd-mysql-perl
+apt-get -y install libdbi-perl
+apt-get -y install libnet-ip-perl
+apt-get -y install libsoap-lite-perl
+apt-get -y install libxml-simple-perl
