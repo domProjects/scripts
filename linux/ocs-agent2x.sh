1d01bbfe253c0e3ccb81d81e18669d481346a7f5
Emmanuel Campait <ecampait@gmail.com>
2018-10-29 21:44:53 +0100

    Update linux/ocs-agent2x.sh

--- linux/ocs-agent2x.sh
+++ linux/ocs-agent2x.sh
@@ -9,6 +9,22 @@ apt-get -y install cpanminus
 apt-get -y install make gcc
 apt-get -y install dmidecode
 apt-get -y install pciutils
+apt-get -y install nmap
+apt-get -y install libcrypt-ssleay-perl
+apt-get -y install libmodule-install-perl
+apt-get -y install libxml-simple-perl
+apt-get -y install libcompress-zlib-perl
+apt-get -y install libnet-ip-perl
+apt-get -y install libwww-perl
+apt-get -y install libdigest-md5-perl
+apt-get -y install libdata-uuid-perl
+apt-get -y install libnet-snmp-perl
+apt-get -y install libproc-pid-file-perl
+apt-get -y install libproc-daemon-perl
+apt-get -y install net-tools
+apt-get -y install libsys-syslog-perl
+apt-get -y install read-edid
+#apt-get -y install smartmontools
 
 #
 cpan -i XML::Simple
@@ -31,12 +47,7 @@ cpan -i Module::Install
 cpan -i Parse::EDID
 
 #
-mkdir -p /usr/local/src/nmap
-cd /usr/local/src/nmap
-
-wget http://nmap.org/dist/nmap-7.70.tar.bz2
-
-tar xvjf nmap-7.70.tar.bz2
-cd nmap-7.70
-
-./configure
+wget https://github.com/OCSInventory-NG/UnixAgent/releases/download/v2.4.2/Ocsinventory-Unix-Agent-2.4.2.tar.gz
+tar –xvzf Ocsinventory-Unix-Agent-2.4.2.tar.gz
+cd Ocsinventory-Unix-Agent-2.4.2
+env PERL_AUTOINSTALL=1 perl Makefile.PL
