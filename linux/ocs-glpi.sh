8d56dec4effca1a5e5a8980a9279f2f1023629c0
Emmanuel Campait <ecampait@gmail.com>
2018-10-29 16:23:07 +0100

    Update linux/ocs-glpi.sh

--- linux/ocs-glpi.sh
+++ linux/ocs-glpi.sh
@@ -96,3 +96,42 @@ a2enconf zz-ocsinventory-restapi
 chown -R www-data:www-data /var/lib/ocsinventory-reports
 
 systemctl reload apache2
+
+#
+cd /tmp/
+
+#
+wget -O teamviewer.zip https://github.com/PluginsOCSInventory-NG/teamviewer/releases/download/${ocsteamviewer}/teamviewer.zip
+if [ $? -ne 0 ]; then
+    echo "Failed to download teamviewer.zip"
+    echo "https://github.com/PluginsOCSInventory-NG/teamviewer/releases/download/${ocsteamviewer}/teamviewer.zip"
+    exit
+fi
+mv teamviewer.zip /usr/share/ocsinventory-reports/ocsreports/download/
+
+#
+wget -O networkshare.zip https://github.com/PluginsOCSInventory-NG/networkshare/releases/download/${ocsnetworkshare}/networkshare.zip
+if [ $? -ne 0 ]; then
+    echo "Failed to download networkshare.zip"
+    echo "https://github.com/PluginsOCSInventory-NG/networkshare/releases/download/${ocsnetworkshare}/networkshare.zip"
+    exit
+fi
+mv networkshare.zip /usr/share/ocsinventory-reports/ocsreports/download/
+
+#
+wget -O officepack.zip https://github.com/PluginsOCSInventory-NG/officepack/releases/download/${ocsofficepack}/officepack.zip
+if [ $? -ne 0 ]; then
+    echo "Failed to download officepack.zip"
+    echo "https://github.com/PluginsOCSInventory-NG/officepack/releases/download/${ocsofficepack}/officepack.zip"
+    exit
+fi
+mv officepack.zip /usr/share/ocsinventory-reports/ocsreports/download/
+
+#
+wget -O security.zip https://github.com/PluginsOCSInventory-NG/security/releases/download/${ocssecurity}/security.zip
+if [ $? -ne 0 ]; then
+    echo "Failed to download security.zip"
+    echo "https://github.com/PluginsOCSInventory-NG/security/releases/download/${ocssecurity}/security.zip"
+    exit
+fi
+mv security.zip /usr/share/ocsinventory-reports/ocsreports/download/
