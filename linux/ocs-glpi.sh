6f5e7fed136b946fcc21df636bb6e1914d7e8465
Emmanuel Campait <ecampait@gmail.com>
2018-10-29 13:11:25 +0100

    Update linux/ocs-glpi.sh

--- linux/ocs-glpi.sh
+++ linux/ocs-glpi.sh
@@ -5,50 +5,32 @@
 # https://github.com/PluginsOCSInventory-NG
 
 #
-ocsteamviewer="1.1"
-ocsnetworkshare="1.0"
-ocsofficepack="2.0"
-ocssecurity="1.1"
 
 #
 clear
 
-#
-cd /tmp/
+# add repository
+add-apt-repository universe
+
+# update apt
+apt-get clean
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+
+# Install prereqs
+apt-get -y install cpanminus
+apt-get -y install make gcc
 
 #
-wget -O teamviewer.zip https://github.com/PluginsOCSInventory-NG/teamviewer/releases/download/${ocsteamviewer}/teamviewer.zip
-if [ $? -ne 0 ]; then
-    echo "Failed to download teamviewer.zip"
-    echo "https://github.com/PluginsOCSInventory-NG/teamviewer/releases/download/${ocsteamviewer}/teamviewer.zip"
-    exit
-fi
-mv teamviewer.zip /usr/share/ocsinventory-reports/ocsreports/download/
+apt-get -y install apache2
+systemctl enable apache2
+systemctl start apache2
 
-#
-wget -O networkshare.zip https://github.com/PluginsOCSInventory-NG/networkshare/releases/download/${ocsnetworkshare}/networkshare.zip
-if [ $? -ne 0 ]; then
-    echo "Failed to download networkshare.zip"
-    echo "https://github.com/PluginsOCSInventory-NG/networkshare/releases/download/${ocsnetworkshare}/networkshare.zip"
-    exit
-fi
-mv networkshare.zip /usr/share/ocsinventory-reports/ocsreports/download/
 
-#
-wget -O officepack.zip https://github.com/PluginsOCSInventory-NG/officepack/releases/download/${ocsofficepack}/officepack.zip
-if [ $? -ne 0 ]; then
-    echo "Failed to download officepack.zip"
-    echo "https://github.com/PluginsOCSInventory-NG/officepack/releases/download/${ocsofficepack}/officepack.zip"
-    exit
-fi
-mv officepack.zip /usr/share/ocsinventory-reports/ocsreports/download/
 
-#
-wget -O security.zip https://github.com/PluginsOCSInventory-NG/security/releases/download/${ocssecurity}/security.zip
-if [ $? -ne 0 ]; then
-    echo "Failed to download security.zip"
-    echo "https://github.com/PluginsOCSInventory-NG/security/releases/download/${ocssecurity}/security.zip"
-    exit
-fi
-mv security.zip /usr/share/ocsinventory-reports/ocsreports/download/
+
+
+
+
 
