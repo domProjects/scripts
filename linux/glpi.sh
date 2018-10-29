f3a3e9de5a04dddcaa374396a2103dc3d222bbab
Emmanuel Campait <ecampait@gmail.com>
2018-10-29 16:07:43 +0100

    Update linux/glpi.sh

--- linux/glpi.sh
+++ linux/glpi.sh
@@ -9,68 +9,6 @@ ocsnetworkshare="1.0"
 ocsofficepack="2.0"
 ocssecurity="1.1"
 
-#
-clear
-
-# add repository
-add-apt-repository universe
-
-# update apt
-apt-get clean
-apt-get -y update
-apt-get -y upgrade
-apt-get -y dist-upgrade
-
-# Install prereqs
-apt-get -y install make gcc
-
-#
-apt-get -y install apache2
-#systemctl enable apache2
-#systemctl start apache2
-
-#
-apt-get -y install build-essential
-apt-get -y install php7.2
-apt-get -y install php7.2-apcu
-apt-get -y install php7.2-curl
-apt-get -y install php7.2-gd
-apt-get -y install php7.2-imap
-apt-get -y install php7.2-intl
-apt-get -y install php7.2-ldap
-apt-get -y install php7.2-mbstring
-apt-get -y install php7.2-mysql
-apt-get -y install php7.2-pspell
-apt-get -y install php7.2-recode
-apt-get -y install php7.2-tidy
-apt-get -y install php7.2-xmlrpc
-apt-get -y install php7.2-xsl
-apt-get -y install php-cas
-apt-get -y install php-gettext
-apt-get -y install php-imagick
-apt-get -y install php-memcache
-apt-get -y install php-pear
-apt-get -y install libapache2-mod-php7.2
-
-# install data base
-#apt-get -y install mysql-server
-#apt-get -y install phpmyadmin
-
-#
-cd /tmp/
-
-#
-wget -O glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz https://github.com/pluginsGLPI/ocsinventoryng/releases/download/${pluginglpiocsversion}/glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz
-
-if [ $? -ne 0 ]; then
-  echo "Failed to download glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz"
-  echo "https://github.com/pluginsGLPI/ocsinventoryng/releases/download/${pluginglpiocsversion}/glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz"
-  exit
-fi
-
-tar -xvf glpi-ocsinventoryng-${pluginglpiocsversion}.tar.gz
-mv ocsinventoryng /var/www/html/glpi/plugins/
-
 
 
 #
@@ -111,9 +49,3 @@ if [ $? -ne 0 ]; then
     exit
 fi
 mv security.zip /usr/share/ocsinventory-reports/ocsreports/download/
-
-
-
-# clean install
-rm -rf /tmp/*
-rm -rf ./glpi.sh*
